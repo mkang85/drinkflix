cba8dc1a0ccf5c7fe364bc9fef495b38b2368817
Michael Kang <michaelkang85@gmail.com>
2018-10-12 20:52:27 -0400

    added omniauth gem and sessions controller and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['username']
+      u.email = auth['info']['email']
+    end
+
+    session[:user_id] = @user.id
+
+    render 'users/index'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,9 @@ ActiveRecord::Schema.define(version: 2018_10_10_134014) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.string "password_digest"
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
